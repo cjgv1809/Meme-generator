38794a7fe096339478279f44a6fa62451cedb099
Carlos Gomes <carlos_gomes1809@hotmail.com>
2023-09-03 11:52:02 -0300

    add initial data and assets to be used and config as well

--- App.tsx
+++ App.tsx
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import HomeScreen from "./screens/HomeScreen";
+import { NativeBaseProvider, extendTheme } from "native-base";
+import CreatorScreen from "./screens/CreatorScreen";
+import AboutScreen from "./screens/AboutScreen";
+import CustomDrawerContent from "./components/CustomDrawerContent";
+
+const Drawer = createDrawerNavigator();
+
+const theme = extendTheme({});
 
 export default function App() {
+  const headerStyle = {
+    headerStyle: { backgroundColor: theme.colors.secondary[600] },
+    headerTintColor: theme.colors.secondary[50],
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={theme}>
+      <NavigationContainer>
+        <Drawer.Navigator
+          drawerContent={({ state, navigation, descriptors }) => (
+            <CustomDrawerContent
+              state={state}
+              navigation={navigation}
+              descriptors={descriptors}
+            />
+          )}
+          initialRouteName="Home"
+        >
+          <Drawer.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              title: "Trending Memes",
+              ...headerStyle,
+            }}
+          />
+          <Drawer.Screen
+            name="Creator"
+            component={CreatorScreen}
+            options={{
+              title: "Meme Generator",
+              ...headerStyle,
+            }}
+          />
+          <Drawer.Screen
+            name="About"
+            component={AboutScreen}
+            options={{
+              title: "About the App",
+              ...headerStyle,
+            }}
+          />
+        </Drawer.Navigator>
+      </NavigationContainer>
+
+      <StatusBar style="light" animated={true} />
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ typings.d.ts
@@ -0,0 +1,10 @@
+interface TrendingMeme {
+  title: string;
+  url: string;
+  created_utc: number;
+}
+
+interface Meme {
+  name: string;
+  image: ImageSourcePropType;
+}
